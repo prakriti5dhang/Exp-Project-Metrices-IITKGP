592c28991beb8d032cb4998329cfa0624125d502
prakriti5dhang <prakritidhang@gmail.com>
2023-05-17 10:27:10 +0530

    project metrices

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -125,7 +125,7 @@ function subex(){
 function viewsolutionb(){
     document.getElementById("viewsol").disabled=false;
     document.getElementById("dispviewbtn").disabled=true;
-    window.scrollBy(0, 1500);
+    window.scrollBy(0, 2000);
 }
 
  /* Exercise 1  project type choice*/
